d54f04f0be93fe1e8757ed16b992f2e27ac594ff
Samuel Wanjiku <samuel@samuelmwangi.co.ke>
2021-06-21 21:33:52 +0300

    Added vite and postcss config

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default ({ command }) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'false',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve('resources/js'),
+        },
+    },
+    plugins: [
+        vue(),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
+    ]
+});
