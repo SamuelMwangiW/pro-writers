2cb0d20aea2aa2675eb3e11133d64c3557587f0c
Samuel Wanjiku <samuel@samuelmwangi.co.ke>
2021-06-21 22:09:52 +0300

    swap out require for import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-require('./bootstrap');
+import '../css/app.css';
+import './bootstrap.js'
 
 // Import modules...
 import { createApp, h } from 'vue';
@@ -11,7 +12,10 @@ createApp({
     render: () =>
         h(InertiaApp, {
             initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
+            resolveComponent: async (name) => {
+                const pages = import.meta.glob('./Pages/**/*.vue');
+                return (await pages[`./Pages/${name}.vue`]()).default;
+            },
         }),
 })
     .mixin({ methods: { route } })
